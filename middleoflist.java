7afefe060255c2be81d08da53d86c4b81c5ca97a
Harshad Patel <33075626+harshadusa1938@users.noreply.github.com>
2017-12-25 10:37:54 -0800

    Add files via upload

---
+++ middleoflist.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class middleoflist
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		ArrayList list=new ArrayList();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add(1,"G");
+		System.out.println(list);
+		
+	}
+
+}
